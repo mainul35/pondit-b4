fa80fa4704b96afdccbce62dd0c60677db73b8c0
mainul35 <mainuls18@gmail.com>
2021-11-24 23:22:39 +0700

    testing

--- src/com/pondit/b4/class8/RunningCommands.java
+++ src/com/pondit/b4/class8/RunningCommands.java
@@ -5,14 +5,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class RunningCommands {
-    public static void main(String[] args) throws IOException {
-        var process = Runtime.getRuntime().exec("git status");
-
-        var is = process.getInputStream();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
-        String s = "";
-        while ((s = bufferedReader.readLine()) != null) {
-            System.out.println(s);
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String[] commands = new String[] {"ls", "git add .", "git commit -m 'committing with java code'"};
+        var runtime = Runtime.getRuntime();
+        for (int i = 0; i < commands.length; i++) {
+            var process = runtime.exec(commands[i]);
+            var is = process.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+            String s = "";
+            while ((s = bufferedReader.readLine()) != null) {
+                System.out.println(s);
+            }
         }
     }
 }
--- src/com/pondit/b4/class8/understanding-git.md
+++ src/com/pondit/b4/class8/understanding-git.md
@@ -3,3 +3,10 @@
 - Git: Version controlling system (Alternative: SVN)
 - Guthub: Online project repository (Alternatives: GitLab, BitBucket)
 
+## Important commands demonstrated
+
+- `git status` : to see status of changes in currently working branch
+- `git add .` : `.` represents all files in current branch. With `git add .` we can add all files to the staged situation (staged means files / changes ready to be committed)
+- `git restore --staged .` : If we need any file to unstage
+- `git commit -m "some commit message"` : We use git commit with some commit message to perform commit. We can only commit the staged files and commits are must before we want to publish (push) some changes to the remote repository.
+- `git push` : We use git push to publish any committed changes in the working directory.
