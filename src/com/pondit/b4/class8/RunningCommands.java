44ca1a78c938d951dbd38502fd6a61b6cdf347c7
mainul35 <mainuls18@gmail.com>
2021-11-25 00:54:52 +0700

    readme updated

--- src/com/pondit/b4/class8/RunningCommands.java
+++ src/com/pondit/b4/class8/RunningCommands.java
@@ -9,14 +9,24 @@ public class RunningCommands {
         String[] commands = new String[] {"ls", "git add .", "git commit -m 'committing with java code'"};
         var runtime = Runtime.getRuntime();
         for (int i = 0; i < commands.length; i++) {
-            var process = runtime.exec(commands[i]);
-            var is = process.getInputStream();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
-            String s = "";
-            while ((s = bufferedReader.readLine()) != null) {
-                System.out.println(s);
+            final var process = runtime.exec(commands[i]);
+            new Thread() {
+                @Override
+                public void run() {
+                    var is = process.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+                    String s = "";
+                    while (true) {
+                        try {
+                            if ((s = bufferedReader.readLine()) == null) break;
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println(s);
+                    }
+                }
+            }.start();
 
-            }
         }
     }
 }
