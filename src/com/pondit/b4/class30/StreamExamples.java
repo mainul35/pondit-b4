af7e593eed0c66b8e082d77a2e66e0c68a573095
mainul35 <mainuls18@gmail.com>
2022-02-12 22:05:14 +0700

    easy example added for streams api

--- src/com/pondit/b4/class30/StreamExamples.java
+++ src/com/pondit/b4/class30/StreamExamples.java
@@ -1,36 +1,63 @@
-//package com.pondit.b4.class30;
-//
-//import static java.util.Comparator.comparing;
-//import static java.util.stream.Collectors.toList;
-//
-//public class StreamExamples {
-//
-//    public static void main(String[] args) {
-//
-//        // Before
-//        List<Dish> lowCaloricDishes = new ArrayList<>();
-//        for(Dish dish: menu) {
-//            if(dish.getCalories() < 400) {
-//                lowCaloricDishes.add(dish);
-//            }
-//        }
-//        Collections.sort(lowCaloricDishes, new Comparator<Dish>() {
-//            public int compare(Dish dish1, Dish dish2) {
-//                return Integer.compare(dish1.getCalories(), dish2.getCalories());
-//            }
-//        });
-//        List<String> lowCaloricDishesName = new ArrayList<>();
-//        for(Dish dish: lowCaloricDishes) {
-//            lowCaloricDishesName.add(dish.getName());
-//        }
-//
-//        // After
-//
-//        List<String> lowCaloricDishesName =
-//                menu.stream()
-//                        .filter(d -> d.getCalories() < 400)
-//                        .sorted(comparing(Dish::getCalories))
-//                        .map(Dish::getName)
-//                        .collect(toList());
-//    }
-//}
+package com.pondit.b4.class30;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+
+public class StreamExamples {
+
+    public static void main(String[] args) {
+        List<Dish> menu = new ArrayList<>();
+
+        // Before
+        List<Dish> lowCaloricDishes = new ArrayList<>();
+        for(Dish dish: menu) {
+            if(dish.getCalories() < 400) {
+                lowCaloricDishes.add(dish);
+            }
+        }
+        Collections.sort(lowCaloricDishes, new Comparator<Dish>() {
+            public int compare(Dish dish1, Dish dish2) {
+                return Integer.compare(dish1.getCalories(), dish2.getCalories());
+            }
+        });
+        List<String> lowCaloricDishesName = new ArrayList<>();
+        for(Dish dish: lowCaloricDishes) {
+            lowCaloricDishesName.add(dish.getName());
+        }
+
+        // After
+
+        lowCaloricDishesName =
+                menu.stream()
+                        .filter(d -> d.getCalories() < 400)
+                        .sorted(comparing(Dish::getCalories))
+                        .map(Dish::getName)
+                        .collect(toList());
+    }
+}
+
+class Dish {
+    private String name;
+    private int calories;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+}
