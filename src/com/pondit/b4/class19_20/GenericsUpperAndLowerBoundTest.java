9a7e1f57c4174aecf4001875d9b7827814d8cce6
mainul35 <mainuls18@gmail.com>
2022-02-20 13:40:53 +0700

    updated generics examples

--- src/com/pondit/b4/class19_20/GenericsUpperAndLowerBoundTest.java
+++ src/com/pondit/b4/class19_20/GenericsUpperAndLowerBoundTest.java
@@ -8,38 +8,27 @@ class GenericsUpperBoundTest <T extends Daughter>{
     List<T> list = new ArrayList<>();
 
     public static void main(String[] args) {
-        GrandPa grandPa = new GrandPa();
-        Daughter daughter = new Daughter();
-        Son son = new Son();
-        GrandChild1 grandChild1 = new GrandChild1();
-        GrandChild2 grandChild2 = new GrandChild2();
-        GrandChild3 grandChild3 = new GrandChild3();
-
-        GenericsUpperBoundTest<Daughter> upperBoundTest = new GenericsUpperBoundTest<>();
-
-        upperBoundTest.list.add(daughter);
-        upperBoundTest.list.add(grandChild2);
-
-        // This will work
+        // This is an exceptional example of using wildcard in upper bound definition. This code will work.
+        // We can call it definition level use case, because we defined the List and populated with value in one go
         List<? extends GrandPa> childrenOfGrandPa = List.of(new GrandPa(), new Daughter(), new GrandChild2());
 
-        // Howfollowings are out of upper bound reach
-//        upperBoundTest.list.add(grandPa);
-//        upperBoundTest.list.add(son);
-//        upperBoundTest.list.add(grandChild1);
-//        upperBoundTest.list.add(grandChild3);
+        List<GrandPa> childrenOfGrandPa2 = new ArrayList<>();
+        childrenOfGrandPa2.add(new Daughter());
+        childrenOfGrandPa2.add(new GrandChild2());
 
-//        List<? extends Daughter> list = new ArrayList<>();
-//        list.add(daughter);
-//        list.add(grandChild2);
-//        list.add(grandPa);
-//        list.add(son);
-//        list.add(grandChild1);
-//        list.add(grandChild3);
+        GenericsUpperBoundTest<Daughter> upperBoundTest = new GenericsUpperBoundTest<>();
+        upperBoundTest.list.add(new Daughter());
+        upperBoundTest.list.add(new GrandChild2());
+
+        // however, followings are out of upper bound reach
+//         upperBoundTest.list.add(new GrandPa());
+//         upperBoundTest.list.add(new Son());
+//         upperBoundTest.list.add(new GrandChild1());
+//         upperBoundTest.list.add(new GrandChild3());
 
     }
 
-    static public <P extends Son> void addSon(P p){
+    public void addSon(List<? extends Son> p){
         System.out.println(p);
     }
 }
@@ -47,19 +36,14 @@ class GenericsUpperBoundTest <T extends Daughter>{
 class GenericsLowerBoundTest <T> {
 
     public static void main(String[] args) {
-        GrandPa grandPa = new GrandPa();
-        Daughter daughter = new Daughter();
-        Son son = new Son();
-        GrandChild1 grandChild1 = new GrandChild1();
-        GrandChild2 grandChild2 = new GrandChild2();
-        GrandChild3 grandChild3 = new GrandChild3();
         List<? super Son> list = new ArrayList<>();
-        list.add(grandChild1);
-        list.add(grandChild3);
-        list.add(son);
-//        list.add(grandChild2);
-//        list.add(grandPa);
-//        list.add(daughter);
+        list.add(new GrandChild1());
+        list.add(new GrandChild3());
+        list.add(new Son());
+        // followings are out of lower bound reach
+//        list.add(new GrandChild2());
+//        list.add(new GrandPa());
+//        list.add(new Daughter());
     }
 
 }
